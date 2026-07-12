417bda80f34691786185b497b3a3ad0b5edbc4a8
Kai <kai@KaideMacBook-Air.local>
2026-07-12 10:15:21 +0800

    SynthPanel: center divider between lang switches and BG button

--- src/components/SynthPanel.tsx
+++ src/components/SynthPanel.tsx
@@ -140,6 +140,8 @@ export default function SynthPanel({ current }: Props) {
           height: 36,
           background: 'rgba(160,185,215,0.4)',
           marginTop: 11,
+          marginLeft: 10,
+          marginRight: 10,
           alignSelf: 'flex-start',
         }} />
 
